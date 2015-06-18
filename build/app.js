9cce62bef4852c73add79250ef00e01bcf87fb38
Karl Groves <karl@karlgroves.com>
2015-06-17 21:16:41 -0400

    WIP accessibility improvements

--- build/app.js
+++ build/app.js
@@ -1,132 +1,166 @@
 var TableComponent = React.createClass({displayName: "TableComponent",
 
-  getInitialState: function() {
-    return {
-      data: [],
-      sortDir: {}
-    };
-  },
+    getInitialState: function () {
+        return {
+            data: [],
+            sortDir: {}
+        };
+    },
 
-  componentDidMount: function(){
-    $.getJSON(this.props.src, {
-      format: "json"
-    }).done(function(data) {
+    componentDidMount: function () {
+        $.getJSON(this.props.src, {
+            format: "json"
+        }).done(function (data) {
 
-      if (this.isMounted()) {
-        this.setState({
-          data: data
-         });
-      }
-    }.bind(this));
-  },
+            if (this.isMounted()) {
+                this.setState({
+                    data: data
+                });
+            }
+        }.bind(this));
+    },
 
-  getColumnNames: function() {
-    if (this.isMounted()){
-      var firstEl = this.state.data[0];
-      return Object.keys(firstEl);
+    getColumnNames: function () {
+        if (this.isMounted()) {
+            var firstEl = this.state.data[0];
+            return Object.keys(firstEl);
+        }
+    },
+
+    sortByColumn: function (array, column, sortDir) {
+        return array.sort(function (a, b) {
+            var x = a[column];
+            var y = b[column];
+            if (sortDir === 'asc') {
+                return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+            } else {
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            }
+        });
+    },
+
+    sort: function (column) {
+        var sortDir = this.state.sortDir;
+        var data = this.state.data;
+        var sortedData = this.sortByColumn(data, column, sortDir[column]);
+        this.setState({data: sortedData});
+        sortDir[column] = (sortDir[column] === 'asc' ? 'dsc' : 'asc');
+        this.setState({sortDir: sortDir});
+        this.props.caption += ' Sorted By ' + ' ' + column + ' ' + sortDir;
+    },
+
+    render: function () {
+        var columns = this.getColumnNames();
+        var data = this.state.data;
+
+        if (this.isMounted()) {
+            return (
+                React.createElement("table", null, 
+                    React.createElement(TableCaption, {caption: this.props.caption, onSort: this.sort}), 
+                    React.createElement("thead", null, 
+                        React.createElement(TableHeader, {onSort: this.sort, columns: columns})
+                    ), 
+                    React.createElement(TableBody, {data: data, columns: columns})
+                )
+            )
+        } else {
+            return (
+                React.createElement("table", null)
+            )
+        }
     }
-  },
-
-  sortByColumn: function(array, column, sortDir) {
-    return array.sort(function(a, b) {
-      var x = a[column];
-      var y = b[column];
-      if (sortDir === 'asc') {
-        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-      } else {
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-      }
-    });
-  },
-
-  sort: function(column) {
-    var sortDir = this.state.sortDir;
-    var data = this.state.data;
-    var sortedData = this.sortByColumn(data, column, sortDir[column]);
-    this.setState({data: sortedData});
-    sortDir[column] = (sortDir[column] === 'asc' ? 'dsc' : 'asc');
-    this.setState({sortDir: sortDir});
-  },
-
-  render: function() {
-    var columns = this.getColumnNames();
-    var data = this.state.data;
-
-    if (this.isMounted()){
-      return (
-        React.createElement("table", null, 
-          React.createElement("thead", null, 
-            React.createElement(TableHeader, {onSort: this.sort, columns: columns})
-          ), 
-          React.createElement(TableBody, {data: data, columns: columns})
-        )
-      )
-    } else {
-      return (
-        React.createElement("table", null)
-      )
-    }
-  }
 });
 
 
 var TableHeader = React.createClass({displayName: "TableHeader",
-  sort: function(column) {
-    return function(event) {
-      var sortDir = this.props.sortDir;
-      this.props.onSort(column, sortDir);
-    }.bind(this);
-  },
+    sort: function (column) {
+        return function (event) {
 
-  render: function() {
-    var columns = this.props.columns;
-    var cell = function() {
-        return columns.map(function(c, i) {
-          return React.createElement("th", {onClick: this.sort(c), key: c}, c);
-        }, this);
-      }.bind(this);
+            var code = event.charCode || event.keyCode,
+                type = event.type;
 
-    return (
-      React.createElement("tr", {key: "headerRow"},  cell(this.props.item) )
-    )
-  }
+            if (type === 'keydown') {
+                var ENTER = 13,
+                    SPACE = 32;
+
+                if ((code !== ENTER) && (code !== SPACE)) {
+                    event.stopPropagation();
+                }
+                else if (code === SPACE) {
+                    event.preventDefault();
+                }
+            }
+
+            var sortDir = this.props.sortDir;
+            console.log(sortDir);
+            console.log(column);
+            this.props.onSort(column, sortDir);
+        }.bind(this);
+    },
+
+    render: function () {
+        var columns = this.props.columns;
+        var cell = function () {
+            return columns.map(function (c, i) {
+                return React.createElement("th", {scope: "col", tabIndex: "0", role: "columnheader", onKeyDown: this.sort(c), onClick: this.sort(c), key: c}, c);
+            }, this);
+        }.bind(this);
+
+
+        return (
+            React.createElement("tr", {key: "headerRow"},  cell(this.props.item) )
+        )
+    }
+});
+
+var TableCaption = React.createClass({displayName: "TableCaption",
+
+    render: function () {
+
+        return (
+            React.createElement("caption", {role: "status", "aria-live": "assertive", "aria-relevant": "all", "aria-atomic": "true"}, this.props.caption)
+        )
+    }
 });
 
 
 var TableBody = React.createClass({displayName: "TableBody",
-  render: function(){
-    var columns = this.props.columns;
-    var data = this.props.data;
+    render: function () {
+        var columns = this.props.columns;
+        var data = this.props.data;
 
-    return (
-      React.createElement("tbody", null, 
-        data.map(function(item, idx){
-          return React.createElement(TableRow, {key: idx, data: item, columns: columns});
+        return (
+            React.createElement("tbody", null, 
+        data.map(function (item, idx) {
+            return React.createElement(TableRow, {key: idx, data: item, columns: columns});
         })
-      )
-    )
-  }
+            )
+        )
+    }
 });
 
 
 var TableRow = React.createClass({displayName: "TableRow",
-  render: function() {
-    var columns = this.props.columns;
-    var data = this.props.data;
-    var td = function(item) {
+    render: function () {
+        var columns = this.props.columns;
+        var data = this.props.data;
+        var td = function (item) {
 
-        return columns.map(function(c, i) {
-          return React.createElement("td", {key: i}, item[c]);
-        }, this);
-      }.bind(this);
+            return columns.map(function (c, i) {
 
-    return (
-      React.createElement("tr", {key: data},  td(data) )
-    )
-  }
+
+
+                return React.createElement("td", {key: i}, item[c]);
+            }, this);
+        }.bind(this);
+
+        return (
+            React.createElement("tr", {key: data},  td(data) )
+        )
+    }
 });
 
 
 React.render(
-  React.createElement(TableComponent, {src: "./data/data.json"}), document.getElementById('table')
+    React.createElement(TableComponent, {caption: "Foo", src: "./data/data.json"}), document.getElementById('table')
 )
--- js/app.js
+++ js/app.js
@@ -1,132 +1,166 @@
 var TableComponent = React.createClass({
 
-  getInitialState: function() {
-    return {
-      data: [],
-      sortDir: {}
-    };
-  },
+    getInitialState: function () {
+        return {
+            data: [],
+            sortDir: {}
+        };
+    },
 
-  componentDidMount: function(){
-    $.getJSON(this.props.src, {
-      format: "json"
-    }).done(function(data) {
+    componentDidMount: function () {
+        $.getJSON(this.props.src, {
+            format: "json"
+        }).done(function (data) {
 
-      if (this.isMounted()) {
-        this.setState({
-          data: data
-         });
-      }
-    }.bind(this));
-  },
+            if (this.isMounted()) {
+                this.setState({
+                    data: data
+                });
+            }
+        }.bind(this));
+    },
 
-  getColumnNames: function() {
-    if (this.isMounted()){
-      var firstEl = this.state.data[0];
-      return Object.keys(firstEl);
+    getColumnNames: function () {
+        if (this.isMounted()) {
+            var firstEl = this.state.data[0];
+            return Object.keys(firstEl);
+        }
+    },
+
+    sortByColumn: function (array, column, sortDir) {
+        return array.sort(function (a, b) {
+            var x = a[column];
+            var y = b[column];
+            if (sortDir === 'asc') {
+                return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+            } else {
+                return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+            }
+        });
+    },
+
+    sort: function (column) {
+        var sortDir = this.state.sortDir;
+        var data = this.state.data;
+        var sortedData = this.sortByColumn(data, column, sortDir[column]);
+        this.setState({data: sortedData});
+        sortDir[column] = (sortDir[column] === 'asc' ? 'dsc' : 'asc');
+        this.setState({sortDir: sortDir});
+        this.props.caption += ' Sorted By ' + ' ' + column + ' ' + sortDir;
+    },
+
+    render: function () {
+        var columns = this.getColumnNames();
+        var data = this.state.data;
+
+        if (this.isMounted()) {
+            return (
+                <table>
+                    <TableCaption caption={this.props.caption} onSort={this.sort} />
+                    <thead>
+                        <TableHeader onSort={this.sort} columns={columns} />
+                    </thead>
+                    <TableBody data={data} columns={columns} />
+                </table>
+            )
+        } else {
+            return (
+                <table></table>
+            )
+        }
     }
-  },
-
-  sortByColumn: function(array, column, sortDir) {
-    return array.sort(function(a, b) {
-      var x = a[column];
-      var y = b[column];
-      if (sortDir === 'asc') {
-        return ((x > y) ? -1 : ((x < y) ? 1 : 0));
-      } else {
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-      }
-    });
-  },
-
-  sort: function(column) {
-    var sortDir = this.state.sortDir;
-    var data = this.state.data;
-    var sortedData = this.sortByColumn(data, column, sortDir[column]);
-    this.setState({data: sortedData});
-    sortDir[column] = (sortDir[column] === 'asc' ? 'dsc' : 'asc');
-    this.setState({sortDir: sortDir});
-  },
-
-  render: function() {
-    var columns = this.getColumnNames();
-    var data = this.state.data;
-
-    if (this.isMounted()){
-      return (
-        <table>
-          <thead>
-            <TableHeader onSort={this.sort} columns={columns} />
-          </thead>
-          <TableBody data={data} columns={columns} />
-        </table>
-      )
-    } else {
-      return (
-        <table></table>
-      )
-    }
-  }
 });
 
 
 var TableHeader = React.createClass({
-  sort: function(column) {
-    return function(event) {
-      var sortDir = this.props.sortDir;
-      this.props.onSort(column, sortDir);
-    }.bind(this);
-  },
+    sort: function (column) {
+        return function (event) {
 
-  render: function() {
-    var columns = this.props.columns;
-    var cell = function() {
-        return columns.map(function(c, i) {
-          return <th onClick={this.sort(c)} key={c}>{c}</th>;
-        }, this);
-      }.bind(this);
+            var code = event.charCode || event.keyCode,
+                type = event.type;
 
-    return (
-      <tr key="headerRow">{ cell(this.props.item) }</tr>
-    )
-  }
+            if (type === 'keydown') {
+                var ENTER = 13,
+                    SPACE = 32;
+
+                if ((code !== ENTER) && (code !== SPACE)) {
+                    event.stopPropagation();
+                }
+                else if (code === SPACE) {
+                    event.preventDefault();
+                }
+            }
+
+            var sortDir = this.props.sortDir;
+            console.log(sortDir);
+            console.log(column);
+            this.props.onSort(column, sortDir);
+        }.bind(this);
+    },
+
+    render: function () {
+        var columns = this.props.columns;
+        var cell = function () {
+            return columns.map(function (c, i) {
+                return <th scope="col" tabIndex="0" role="columnheader" onKeyDown={this.sort(c)}  onClick={this.sort(c)} key={c}>{c}</th>;
+            }, this);
+        }.bind(this);
+
+
+        return (
+            <tr key="headerRow">{ cell(this.props.item) }</tr>
+        )
+    }
+});
+
+var TableCaption = React.createClass({
+
+    render: function () {
+
+        return (
+            <caption role="status" aria-live="assertive" aria-relevant="all" aria-atomic="true">{this.props.caption}</caption>
+        )
+    }
 });
 
 
 var TableBody = React.createClass({
-  render: function(){
-    var columns = this.props.columns;
-    var data = this.props.data;
+    render: function () {
+        var columns = this.props.columns;
+        var data = this.props.data;
 
-    return (
-      <tbody>
-        {data.map(function(item, idx){
-          return <TableRow key={idx} data={item} columns={columns}/>;
+        return (
+            <tbody>
+        {data.map(function (item, idx) {
+            return <TableRow key={idx} data={item} columns={columns}/>;
         })}
-      </tbody>
-    )
-  }
+            </tbody>
+        )
+    }
 });
 
 
 var TableRow = React.createClass({
-  render: function() {
-    var columns = this.props.columns;
-    var data = this.props.data;
-    var td = function(item) {
+    render: function () {
+        var columns = this.props.columns;
+        var data = this.props.data;
+        var td = function (item) {
 
-        return columns.map(function(c, i) {
-          return <td key={i}>{item[c]}</td>;
-        }, this);
-      }.bind(this);
+            return columns.map(function (c, i) {
 
-    return (
-      <tr key={data}>{ td(data) }</tr>
-    )
-  }
+
+
+                return <td key={i}>{item[c]}</td>;
+            }, this);
+        }.bind(this);
+
+        return (
+            <tr key={data}>{ td(data) }</tr>
+        )
+    }
 });
 
 
 React.render(
-  <TableComponent src="./data/data.json" />, document.getElementById('table')
+    <TableComponent caption="Foo" src="./data/data.json" />, document.getElementById('table')
 )
